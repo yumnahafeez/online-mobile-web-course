e1e087b9d8ccfd39497ea015079d67acda71de3c
Yumna hafeez <yumnahafeez04@outlook.com>
2020-06-27 10:54:01 -0700

    assigment 7 first commit

---
+++ assignment_7/app.js
@@ -0,0 +1,17 @@
+//function for displaying values
+function dis(val)
+{
+document.getElementById("edu").value+=val
+}
+//function for evaluation
+function solve()
+{
+let x = document.getElementById("edu").value
+let y = eval(x)
+document.getElementById("edu").value = y
+}
+//function for clearing the display
+function clr()
+{
+document.getElementById("edu").value = ""
+}
---
+++ assignment_7/node.js
@@ -0,0 +1,19 @@
+<script>
+//function for displaying values
+function dis(val)
+{
+document.getElementById("edu").value+=val
+&nbsp;}
+//function for evaluation
+function solve()
+{
+let x = document.getElementById("edu").value
+let y = eval(x)
+document.getElementById("edu").value = y
+}
+//function for clearing the display
+function clr()
+{
+document.getElementById("edu").value = ""
+}
+</script>
